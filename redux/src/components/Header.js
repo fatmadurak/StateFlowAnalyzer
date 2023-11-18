cbc16726ee9f3d7135add091f33e7082a86a55d0
fatmadurak <fatmabadillidurak@gmail.com>
2023-11-18 17:48:29 +0300

    added header component and header style

--- redux/src/components/Header.js
+++ redux/src/components/Header.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import "../assets/styles/Header.css"
 const Header = () => {
   return (
-    <div>Header</div>
+   <>
+   <div className='header-container'>
+    
+    <div className='logo'>
+        UYGULAMA
+
+    
+     </div>
+
+    <div className='header-wrapper'>
+        <select name='' id=''>
+            <option value="asc">ARTAN</option>
+            <option value="desc">AZALAN</option>
+        </select>
+   
+
+   
+        <input type="" name="" value="" placeholder='Arama Yapınız:'/>
+      
+     <button type="">Modal Ekle</button>
+   
+     </div>
+
+   
+
+   </div>
+   
+   
+   
+   </>
   )
 }
 
