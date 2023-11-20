9bfecfd60ce9c637c5ce7ea6b130450f0fc98acc
fatmadurak <fatmabadillidurak@gmail.com>
2023-11-20 11:31:21 +0300

    added modal and modal styles

---
+++ redux/src/components/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../assets/styles/Modal.css"
+
+function Modal() {
+  return (
+    <div className='modal'>
+
+  <div className='modal-wrapper'>
+    Modal
+  </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Modal
--- redux/src/redux-store/Store.js
+++ redux/src/redux-store/Store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { dataSlice } from './DataSlice'
-import { modalSlice } from './ModalSlice'
+import  dataSlice  from './DataSlice'
+import  modalSlice  from './ModalSlice'
 
 export default configureStore({
   reducer: {
   
-    data:{dataSlice},
-    modal:{modalSlice},
+    data:dataSlice,
+    modal:modalSlice,
 
   },
 })
